bfb4a51ba1999c363059fc199cb1c6fb9488904a
Lothar Maisenbacher <lothar.maisenbacher@berkeley.edu>
2023-03-10 02:22:07 +0000

    Added install scripts that copy bit files to /opt/redpitaya/fpga.

---
+++ scripts/install_z20.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script might needs to be run as root (`sudo -i` or `su root`), not only with `sudo`,
+# in order for the `rw` and `ro` commands to be available.
+# Make '/opt/redpitaya' writeable
+rw
+cp ../build/rp_lockbox_carter_z20.bit /opt/redpitaya/fpga/rp_lockbox_carter.bit
+# Make '/opt/readpitaya' read-only
+ro
